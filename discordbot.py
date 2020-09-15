d9fcab219d6f3de5d6fe823f27cbcdd47615ec2f
tatudon-321 <69696560+tatudon-321@users.noreply.github.com>
2020-09-16 01:10:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,14 +1,22 @@
-from discord.ext import commands
+import discord
+import datetime
+import asyncio
+import random
+import json
+import urllib.request
+import urllib.parse
+import re
 import os
 import traceback
-import discord, os
-from discord.ext import tasks
-from discord.ext import commands 
-from os.path import join, dirname
-from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix='/')
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="k!",help_command=commands.HelpCommand(command_attrs={'hidden': True}))
+client = discord.Client()
+
 token = os.environ['DISCORD_BOT_TOKEN']
+members = {
+    "たつどん": 'https://media.discordapp.net/attachments/741658483711279175/755460447834210424/image0.jpg',}
 
 
 @bot.event
