889c218bfbf8ea01d3c7cf51680282053610095d
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-09-24 07:34:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,14 +17,16 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     embed = discord.Embed(title='pingの測定結果', description='PONG!', color=0x000000)
     embed.add_field(name='結果', value=f'{ctx.bot.latency * 1000} ms')
-    
     await ctx.send(embed=embed)
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pangpang!!paaaaaaan!paaaaaan!;')    
 
 @bot.command()
 async def say(ctx, *, text):
     await ctx.send(text)
+
+@bot.event
+async def on_ready():
+    activity = discord.Game(name="t!help\n prefix→t! !t t. tatu\n owner @たつどん#2239\n 公式サーバー→作成中", type=3)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+    print("Bot is ready!")
     
 bot.run(token)
