0c680010489432cd53e2057c51c4cb41d7972ef1
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-09-25 18:34:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,57 +1,6 @@
-# -*- coding: utf-8 -*- #
-
-import discord
-from discord.ext import commands,tasks
-import json
-import random
-import wikipedia
-import wikidata.client
-from PIL import Image, ImageDraw, ImageFont
-import time
-import asyncio
-import datetime
-import pickle
-import sys
-import platform
-import re
-from twitter import *
-from dateutil.relativedelta import relativedelta as rdelta
-import traceback
+from discord.ext import commands
 import os
-import shutil
-import pytz
-import sqlite3
-import aiohttp
-
-#textto etc
-import m10s_util as ut
-from apple_util import AppleUtil
-from l10n import TranslateHandler, LocalizedContext
-#tokens
-import config
-#cog
-from cogs import m10s_music
-from cogs import m10s_info
-from cogs import m10s_owner
-from cogs import m10s_settings
-from cogs import m10s_manage
-from cogs import m10s_levels
-from cogs import m10s_tests
-from cogs import m10s_gcoms
-from cogs import m10s_search
-from cogs import m10s_other
-from cogs import m10s_games
-from cogs import P143_jyanken
-from cogs import nekok500_mee6
-from cogs import syouma
-from cogs import pf9_symmetry
-from cogs import apple_foc
-from cogs import m10s_gban
-from cogs import m10s_bmail
-from cogs import m10s_auth_wiz
-
-"""import logging
-logging.basicConfig(level=logging.DEBUG)"""
+import traceback
 
 
 bot = commands.Bot(command_prefix=('t!','t.'))
