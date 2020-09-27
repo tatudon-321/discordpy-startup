d792e717192000e7884f901e75e6647758f06071
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-09-28 01:09:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,8 +9,8 @@ token = os.environ['DISCORD_BOT_TOKEN']
 bot.owner_id = 541290054447005706
 
 #エラーログ出すやつ
-#@bot.event
-#async def on_command_error(ctx, error):
+@bot.event
+async def on_command_error(ctx, error):
     #orig_error = getattr(error, "original", error)
     #error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     #await ctx.send(error_msg)
@@ -33,24 +33,11 @@ bot.remove_command("help")
 
 @bot.command()
 async def help(ctx):
-    await ctx.send
-    embed = discord.Embed(title="Hi!!",description="How are you?")
-    await channel.send(embed=embed)
-
-
-@bot.command()
-async def discordpy(ctx):
-    embed = discord.Embed(description='a')
-    await ctx.channel.send(embed=embed)
+    await ctx.send('コード書き込み中…')
 
 @bot.command()
 async def invite(ctx):
     await ctx.send('このbotの招待url|https://discord.com/api/oauth2/authorize?client_id=728281988892721254&permissions=0&scope=bot')
-    
-@bot.event
-async def on_ready(): # botが起動したときに動作する処理
-    print('bot is ready!!')
-    await client.change_presence(activity=discord.Game(name="製作者|たつどん#2239", type=1))
     
 @bot.command()
 async def bottles(ctx, amount: typing.Optional[int] = 99, *, liquid="beer"):
