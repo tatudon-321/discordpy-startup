1bbd1a39eddaa93122ab0c5637663259e1edb216
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-09-30 07:59:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -69,11 +69,51 @@ async def update(ctx):
     
 async def is_owner(ctx):
     return ctx.author.id == 316026178463072268
+ @commands.command(pass_context=True, hidden=True, name='eval')
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
 
-@bot.command(name='eval')
-@commands.check(is_owner)
-async def _eval(ctx, *, code):
-    """A bad example of an eval command"""
-    await ctx.send(eval(code))
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
     
 bot.run(token)
