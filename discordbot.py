50b940db599e7a5c7533df52f400dca40153ae7e
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-09-30 22:10:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -95,6 +95,11 @@ async def on_message(message):
     if "!じゃんけん" in message.content:
         word_list = ["グー","チョキ","パー"]
         await message.channel.send(random.choice(word_list))
+        
+@bot.command()
+async def jan(ctx):
+    word_list = ["ぐー","ちょき","ぱー"]
+    await ctx.send(random.choice(word.list))
 
     
     
