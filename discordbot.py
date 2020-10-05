06de258f1fa4aad4b4d84214abe5ea3ed1c80692
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-10-06 07:27:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -28,10 +28,6 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
     
-@bot.command()
-async def dice(ctx):
-    dice = random.randint(1, 6) #出る目を指定
-    await message.send_message(message.channel, str(dice))
     
 @bot.command()
 async def embed(ctx, *, text):
