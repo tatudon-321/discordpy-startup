c0eed21f23de5c4bdeb74dec99e77f0ed842f693
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-10-08 16:10:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -43,6 +43,18 @@ async def on_command_error(ctx, error):
     #embed = discord.Embed(description=f'**実行者:{ctx.author}**', color=0x800000)
     #await ctx.send(embed=embed)
     
+@bot.event
+async def on_message(message):
+    if message.content.startswith('$greet'):
+        channel = message.channel
+        await channel.send('Say hello!')
+
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send('Hello {.author}!'.format(msg))
+    
 @bot.command()
 async def embed(ctx, *, text):
     embed = discord.Embed(description=text, color=0x800000)
