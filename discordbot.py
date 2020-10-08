fb0417953ec1f144c1e91ca45646f7b7a4cf0dcc
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-10-08 14:24:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,13 +29,13 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
     
 @bot.command()
-async def kick(ctx, member: discord.Member, *, reason=None):
+async def kick(ctx, member: discord.Member, *, reason=f'実行者:{ctx.author} , by優姫-ゆき-):
     await member.kick (reason=reason)
     embed = discord.Embed(description=f'**実行者:{ctx.author}**', color=0x888000)
     await ctx.send(embed=embed)
     
 @bot.command()
-async def ban(ctx, member: discord.Member, *, reason=None):
+async def ban(ctx, member: discord.Member, *, reason=f'実行者:{ctx.author} , by優姫-ゆき-'):
     await member.ban (reason=reason)
     embed = discord.Embed(description=f'**実行者:{ctx.author}**', color=0x800000)
     await ctx.send(embed=embed)
