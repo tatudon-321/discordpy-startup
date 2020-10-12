faa51c0b5bc162e0b08e35d0e689848f2232a8ad
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-10-12 20:24:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -63,7 +63,7 @@ async def wait(ctx):
         return m.content == 'test' and m.channel == channel
     
     msg = await bot.wait_for('message', check=check)
-    await channel.send('{.author}さん。成功です。'.format(msg))
+    await channel.send('Hello {.author}!'.format(msg))
         
 #@bot.event 
 #async def on_message(message):
