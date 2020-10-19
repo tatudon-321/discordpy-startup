df287e84a9c6490736a8c3f9a0f7733147551f4d
tatudon <69696560+tatudon-321@users.noreply.github.com>
2020-10-19 19:21:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -51,53 +51,7 @@ async def sinfo(ctx):
     embed.set_thumbnail(url=ctx.guild.icon_url)
     await ctx.send(embed=embed)
     
-@bot.command()
-async def eval_fn(ctx, *, cmd):
-    """Evaluates input.
-    Input is interpreted as newline seperated statements.
-    If the last statement is an expression, that is the return value.
-    Usable globals:
-      - `bot`: the bot instance
-      - `discord`: the discord module
-      - `commands`: the discord.ext.commands module
-      - `ctx`: the invokation context
-      - `__import__`: the builtin `__import__` function
-    Such that `>eval 1 + 1` gives `2` as the result.
-    The following invokation will cause the bot to send the text '9'
-    to the channel of invokation and return '3' as the result of evaluating
-    >eval ```
-    a = 1 + 2
-    b = a * 2
-    await ctx.send(a + b)
-    a
-    ```
-    """
-    fn_name = "_eval_expr"
 
-    cmd = cmd.strip("` ")
-
-    # add a layer of indentation
-    cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
-
-    # wrap in async def body
-    body = f"async def {fn_name}():\n{cmd}"
-
-    parsed = ast.parse(body)
-    body = parsed.body[0].body
-
-    insert_returns(body)
-
-    env = {
-        'bot': ctx.bot,
-        'discord': discord,
-        'commands': commands,
-        'ctx': ctx,
-        '__import__': __import__
-    }
-    exec(compile(parsed, filename="<ast>", mode="exec"), env)
-
-    result = (await eval(f"{fn_name}()", env))
-    await ctx.send(result)
    
     
 #@bot.command()
